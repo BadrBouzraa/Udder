a68db8336073598f3677eb317565aa46477799f1
Badr Bouzraa <badr.bouzraa@hotmail.de>
2019-11-21 14:30:44 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Badr Bouzraa on 21.11.19.
+//  Copyright © 2019 Badr Bouzraa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
